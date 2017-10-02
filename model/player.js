7b751967e1c9717ab528c2976812134ffe765d51
Steven Washington <washington.steven@gmail.com>
2017-10-01 21:41:28 -0400

    Basic talking.

--- model/player.js
+++ model/player.js
@@ -10,6 +10,7 @@ class Player {
 
     command.addSubscriber('look', () => { this.doLook() });
     command.addSubscriber('name', (cmd, args) => { this.namePlayer(args) });
+    command.addSubscriber('talk', (cmd, args) => { this.doTalk(args) });
   }
   doLook() {
     if (this.currentTile) {
@@ -24,6 +25,30 @@ class Player {
       console.log(`Your name is now ${this.name}`);
     } else {
       console.log(`Your name is ${this.name}`);
+    }
+  }
+  doTalk(args) {
+    if (!this.currentTile.npcs || this.currentTile.npcs.length == 0) {
+      console.log("There is no one here for you to talk to");
+      return;
+    }
+
+    if (!args || args.length == 0) {
+      console.log('The following people are in the area: ');
+      for (let i = 0; i < this.currentTile.npcs.length; i++) {
+        const npc = this.currentTile.npcs[i];
+        console.log(`\t[${i+1}] ${npc.name}`);
+      }
+    } else {
+      let npcIndex = +args[0];
+      //re: these checks - remember that npcIndex given via prompt is 1-based
+      if (isNaN(npcIndex) || npcIndex < 1 || npcIndex > this.currentTile.npcs.length) {
+        console.log('Invalid NPC id');
+      } else {
+        npcIndex -= 1;
+        const npc = this.currentTile.npcs[npcIndex];
+        console.log(`${npc.name} says "${npc.responses.default}"`);
+      }
     }
   }
 }
